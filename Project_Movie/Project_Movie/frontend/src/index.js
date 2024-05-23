4c48a9256a23a421bd1201b47c398123f54d994e
MYNAMEISHAO <143887136+MYNAMEISHAO@users.noreply.github.com>
2024-05-23 21:24:41 +0700

    Update index.js

--- Project_Movie/Project_Movie/frontend/src/index.js
+++ Project_Movie/Project_Movie/frontend/src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "https://moviebooking-hazel.vercel.app/";
+axios.defaults.baseURL = "https://food-order-fa41.onrender.com";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
