7ce890368da78264188af5c0ad37332c744c6688
MYNAMEISHAO <143887136+MYNAMEISHAO@users.noreply.github.com>
2024-05-24 01:59:24 +0700

    Update index.js

--- Project_Movie/Project_Movie/frontend/src/index.js
+++ Project_Movie/Project_Movie/frontend/src/index.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import { Provider } from "react-redux";
 import { store } from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "https://booking-web-beige.vercel.app/";
+axios.defaults.baseURL = "https://booking-web-beige.vercel.app";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
